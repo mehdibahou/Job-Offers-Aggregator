c6d386fc8b2d453972deeefc4903957bc829a2be
mehdi bahou <bahoubuisness@gmail.com>
2023-10-07 13:20:05 +0100

    fix issues

--- UI/utils/checkPermissions.js
+++
@@ -1,9 +0,0 @@
-import { UnauthorizedError } from "../errors";
-
-const chechPermissions = (requestUser, resourceUserId) => {
-  if (requestUser.role === "admin") return;
-  if (requestUser.userId === resourceUserId.toString()) return;
-  UnauthorizedError("Not authorized to access this route");
-};
-
-export default chechPermissions;
--- UI/utils/scroller.jsx
+++
@@ -1,7 +0,0 @@
-const scrollIntoView = (id) => {
-  const element = document.getElementById(id);
-  if (element) {
-    element.scrollIntoView({ behavior: "smooth" });
-  }
-};
-export default scrollIntoView;
